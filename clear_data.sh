8a462469028ec88b2789280240abff6243effcc3
Oliver Hills <oli@deathbycomputers.co.uk>
2016-05-01 17:21:36 +0800

    clear config script

---
+++ clear_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ' ' >saved_data/d2
+echo ' ' >saved_data/dilbert
+echo ' ' >saved_data/dr
+echo ' ' >saved_data/oglaf
+echo ' ' >saved_data/pa
+echo ' ' >saved_data/qc
+echo ' ' >saved_data/satw
+echo ' ' >saved_data/xkcd
+echo '1033' >saved_data/oots
+echo '918' >saved_data/lfg
